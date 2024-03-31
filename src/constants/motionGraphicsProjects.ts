799665b1b5bed1d115d9357614922356d631bef6
Mica Balza <mica.balza@gmail.com>
2024-03-31 14:04:51 -0300

    feat: motion graphic projects upload

---
+++ src/constants/motionGraphicsProjects.ts
@@ -0,0 +1,62 @@
+const caixaBank = [
+  {
+    type: "cover",
+    name: "CaixaBank",
+    country: "España",
+    description: "PROJECT SCOPE",
+    goal: "Realización de video corporativo, edición de video y elementos gráficos.",
+  },
+  {
+    type: "video",
+    videoUrl: "https://www.youtube.com/embed/JdKDHn6cIsA?si=AXt3rihawjigBipQ",
+  },
+];
+
+const imagin = [
+  {
+    type: "cover",
+    name: "Imagin",
+    country: "España",
+    description: "PROJECT SCOPE",
+    goal: "Realización de video corporativo, edición de video y elementos gráficos.",
+  },
+  {
+    type: "video",
+    videoUrl: "https://youtube.com/embed/fLtYRseUnG0?si=YYIhQbjFxTn4XQkA",
+  },
+];
+
+const olistic = [
+  {
+    type: "cover",
+    name: "Olistic",
+    country: "España",
+    description: "PROJECT SCOPE",
+    goal: "Realización de video corporativo, edición de video y elementos gráficos.",
+  },
+  {
+    type: "video",
+    videoUrl: "https://www.youtube.com/embed/GhMgKtQY1_c?si=j4JSCMlXXpitBfV-",
+  },
+];
+
+const tajMahal = [
+  {
+    type: "cover",
+    name: "Taj-Mahal",
+    country: "España",
+    description: "PROJECT SCOPE",
+    goal: "Realización de video corporativo, edición de video y elementos gráficos.",
+  },
+  {
+    type: "video",
+    videoUrl: "https://www.youtube.com/embed/19p6ci3TToc?si=OPMqh5i_AchAwcRF",
+  },
+];
+
+export const MOTION_GRAPHICS_PROJECTS: Record<string, any> = {
+  tajMahal,
+  imagin,
+  olistic,
+  caixaBank,
+};
