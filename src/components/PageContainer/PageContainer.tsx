a13992665655f735be74be2b1b7c62d036754c61
Mica Balza <mica.balza@gmail.com>
2024-04-24 09:43:18 -0300

    feat: test fixed navbar

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,5 +1,6 @@
-import React, { ReactNode } from "react";
 import { motion } from "framer-motion";
+import { ReactNode } from "react";
+import styles from "./styles.module.css";
 
 interface Props {
   children: ReactNode;
@@ -12,7 +13,7 @@ const PageContainer = ({ children, className }: Props) => {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className={className}
+      className={`${className || ""} ${styles.pageContainer}`}
     >
       {children}
     </motion.div>
