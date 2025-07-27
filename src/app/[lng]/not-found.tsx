2bf95ca4af0b7cf0d755e4c02b45b0dd7f054978
Mica Balza <mica.balza@gmail.com>
2025-07-27 11:32:19 -0300

    style: update footer styles for improved consistency across the services page, including background color adjustments

---
+++ src/app/[lng]/not-found.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useTranslation } from "@/i18n/client";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import styles from "./not-found.module.css";
+
+export default function NotFound() {
+  const { lng } = useParams();
+  const { t } = useTranslation(lng as string, "not-found");
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <div>
+          <Image
+            src="/static/images/404.png"
+            alt="Under Construction"
+            width={400}
+            height={300}
+            className={styles.image}
+          />
+        </div>
+        <h2 className="text-purple">{t("title")}</h2>
+        <p className="text-purple">{t("description")}</p>
+      </div>
+    </div>
+  );
+}
