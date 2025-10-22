3b3937229c8eb71bc59f406ec38d963ee7b7df5a
Mica Balza <mica.balza@gmail.com>
2025-10-22 02:08:22 -0300

    Implement video overlay with play button in HeroSection component, enabling autoplay functionality. Update styles for improved user interaction and visual consistency.

--- src/components/PharmaceuticalSections/HeroSection/index.tsx
+++ src/components/PharmaceuticalSections/HeroSection/index.tsx
@@ -1,5 +1,7 @@
 import Button from "@/components/Button";
 import { useTranslation } from "@/i18n/client";
+import Image from "next/image";
+import { useState } from "react";
 import { HERO_SECTION_VIDEO_URLS } from "./constants";
 import styles from "./styles.module.css";
 
@@ -10,21 +12,52 @@ interface HeroSectionProps {
 
 export default function HeroSection({ lng, onButtonClick }: HeroSectionProps) {
   const { t } = useTranslation(lng, "health-services");
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+
+  const playVideo = () => {
+    setIsVideoPlaying(true);
+  };
 
   return (
     <section className={styles.section}>
       <div className={styles.heroContent}>
         <div className={styles.videoContainer}>
+          {!isVideoPlaying && (
+            <div className={styles.videoOverlay} onClick={playVideo}>
+              <div className={styles.videoBlack} />
+              <Image
+                src="/static/images/pharmaceutical-services/video-placeholders/main.jpg"
+                alt="Health Services Video"
+                fill
+                className={styles.videoPlaceholder}
+                style={{ objectFit: "cover" }}
+              />
+              <Image
+                src="/static/images/play.svg"
+                alt="Play video"
+                width={120}
+                height={127}
+                className={styles.videoPlay}
+              />
+            </div>
+          )}
           <iframe
             className={styles.video}
             src={
-              HERO_SECTION_VIDEO_URLS[
-                lng as keyof typeof HERO_SECTION_VIDEO_URLS
-              ] || HERO_SECTION_VIDEO_URLS.en
+              isVideoPlaying
+                ? `${
+                    HERO_SECTION_VIDEO_URLS[
+                      lng as keyof typeof HERO_SECTION_VIDEO_URLS
+                    ] || HERO_SECTION_VIDEO_URLS.en
+                  }&autoplay=1`
+                : HERO_SECTION_VIDEO_URLS[
+                    lng as keyof typeof HERO_SECTION_VIDEO_URLS
+                  ] || HERO_SECTION_VIDEO_URLS.en
             }
             title="Health Services Video"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen
+            style={{ display: isVideoPlaying ? "block" : "none" }}
           />
         </div>
         <div className={styles.textContent}>
