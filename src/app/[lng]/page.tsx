1533ea3c9977fe0e74e66ba65dd449d1a3aa0ba3
Mica Balza <mica.balza@gmail.com>
2025-11-03 11:18:16 -0300

    fix: dynamically import Hero component for improved loading performance

--- src/app/[lng]/page.tsx
+++ src/app/[lng]/page.tsx
@@ -3,17 +3,22 @@ import { use } from "react";
 
 import Clients from "@/components/Clients";
 import ContactUs from "@/components/ContactUs";
-import Hero from "@/components/Hero";
 import PageContainer from "@/components/PageContainer";
 import PharmaceuticalServices from "@/components/PharmaceuticalServices";
 import Reviews from "@/components/Reviews";
 import WhatWeDo from "@/components/WhatWeDo";
 import WhoWeAre from "@/components/WhoWeAre";
+import dynamic from "next/dynamic";
 import styles from "./page.module.css";
 
 type tParams = {
   lng: string;
 };
+
+const Hero = dynamic(() => import("@/components/Hero"), {
+  loading: () => <div style={{ height: "80vh", background: "var(--nude)" }} />,
+  ssr: false,
+});
 
 export default function Home(props: { params: Promise<tParams> }) {
   const params = use(props.params);
