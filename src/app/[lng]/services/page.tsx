7cd73a0fff002600f87faf7cd1f360cf984287b4
Mica Balza <mica.balza@gmail.com>
2025-11-04 07:26:40 -0300

    fix: remove unused titleMobile styles and adjust title display logic

--- src/app/[lng]/services/page.tsx
+++ src/app/[lng]/services/page.tsx
@@ -24,11 +24,11 @@ export default function Page(props: { params: Promise<tParams> }) {
       <div className={styles.hero}>
         <p className={`text-purple h2 ${styles.title}`}>
           <span className={styles.titleDesktop}>{t("title")}</span>
-          <span className={styles.titleMobile}>
+          {/* <span className={styles.titleMobile}>
             {t("title").split(" ").slice(0, 3).join(" ")}
             <br />
             {t("title").split(" ").slice(3).join(" ")}
-          </span>
+          </span> */}
         </p>
         <div className={styles.imageContainer}>
           <img
