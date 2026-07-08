02f0473fd0fc1a33fada246ce6cf8d573445f0e8
natbalza <holanatbalza@gmail.com>
2026-07-08 13:26:48 +0200

    Update serviceLinks in ServicesBoard component
    Replaced 'less-tm' and 'fluid' with 'podcast-videos' in serviceLinks.

--- src/components/PharmaceuticalSections/ServicesBoard/index.tsx
+++ src/components/PharmaceuticalSections/ServicesBoard/index.tsx
@@ -12,14 +12,11 @@ export default function ServicesBoard({ lng }: ServicesBoardProps) {
 
   const serviceLinks = [
   { key: "item1", url: "institutional-corporate-videos" },
-  { key: "item2", url: "product-launch" },
-  { key: "item3", url: "tutorial-training" },
-  { key: "item4", url: "promotional-videos" },
-  { key: "item5", url: "events-conferences" },
-  { key: "item6", url: "less-tm" },
-  { key: "item7", url: "fluid" },
-  { key: "item8", url: "neuro-swan" },
-  { key: "item9", url: "video-podcast" },
+    { key: "item2", url: "product-launch" },
+    { key: "item3", url: "tutorial-training" },
+    { key: "item4", url: "promotional-videos" },
+    { key: "item5", url: "events-conferences" },
+    { key: "item6", url: "podcast-videos" },
   ];
 
   return (
